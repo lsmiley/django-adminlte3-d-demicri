4ced7ba8ceaf05bb4555594ab504f1f32abc84de
lsmiley <lsmiley@us.ibm.com>
2021-11-05 11:12:33 -0400

    Allowed Host Update
    Allowed Host Update

--- django_adminlte3/settings.py
+++ django_adminlte3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k$v*ssgjspcgakja4u4yo-$=e6^#!wkns4rh6be7fd#m(ivg8('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://django-adminlte-3-d-demicri-git-lsmiley-dev.apps.sandbox.x8i5.p1.openshiftapps.com/', '127.0.0.1']
 
 
 # Application definition
